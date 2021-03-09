a5f3ffd4f8a69a4d85300420838628102d7f517a
psalvi <salvipablo@gmail.com>
2021-03-09 16:38:24 -0300

    Limpieza de variables
    Resolve: T#3

--- scripts.js
+++ scripts.js
@@ -1,12 +1,6 @@
 const buttonMenu = document.getElementById('barraMenu-accion');
 const mainMenu = document.getElementById('mainMenu');
 
-const itemMainMenuIndex = document.getElementById('main-menu-item__index');
-const itemMainMenuWe = document.getElementById('main-menu-item__we');
-const itemMainMenuServices = document.getElementById('main-menu-item__services');
-const itemMainMenuContact = document.getElementById('main-menu-item__contact');
-
-
 buttonMenu.addEventListener('click', () => {
     if (mainMenu.className == "main-menu main-menu-hidden") {
         console.log('Click');
